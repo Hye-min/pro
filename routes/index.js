442fdd4d18e3d6702fb6ecf22cc6636c735f23d9
Hyemin <heamin0@naver.com>
2015-11-26 13:23:59 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '하영이바보' });
+  res.render('index', { title: '하영♥인용' });
 });
 
 module.exports = router;
